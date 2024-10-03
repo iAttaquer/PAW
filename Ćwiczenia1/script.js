a671a59b12a8268cad4638115da1587f7c412497
iAttaquer <iAttaquer@proton.me>
2024-10-03 09:22:37 +0200

    feat: add first excercise to 1-st lesson

--- Ćwiczenia1/script.js
+++ Ćwiczenia1/script.js
@@ -1,3 +1,8 @@
 (function () {
-  //TODO
-})();
+  const button1 = document.getElementById("ex1_button");
+  const output = document.getElementById("ex1_content");
+  button1.addEventListener("click", () => {
+    output.innerHTML = Array.from(Array(10).keys()).join(", ");
+  });
+})
+();
