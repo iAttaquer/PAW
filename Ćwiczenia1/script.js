f51b51b35eeb3f3bdc433bfd00297eda0b57db3c
iAttaquer <iAttaquer@proton.me>
2024-10-03 09:45:06 +0200

    feat: add second excercise to 1-st lesson

--- Ćwiczenia1/script.js
+++ Ćwiczenia1/script.js
@@ -6,3 +6,30 @@
   });
 })
 ();
+(function () {
+  function containsLetters(str) {
+    return /[a-zA-Z]/.test(str);
+  }
+  function containsSpecialCharacters(str) {
+    return /[^a-zA-Z0-9]/.test(str);
+  }
+  const input = document.getElementById("ex2_text");
+  const output = document.getElementById("ex2_content");
+  let message = "";
+  input.addEventListener("input", (e) => {
+    message = "";
+    if (e.target.value.length != 9) {
+      message += "Długość numeru musi być równa 9\n";
+    }
+    if (containsLetters(e.target.value)) {
+      message += "Numer nie może zawierać liter\n";
+    }
+    if (containsSpecialCharacters(e.target.value)) {
+      message += "Numer nie może zawierać znaków specjalnych\n";
+    }
+    if (message === "") {
+      message = "Numer telefonu jest poprawny";
+    }
+    output.innerText = message;
+  })
+  })();
